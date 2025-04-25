74819a3992f570f465481468d2aef82d321d5c1f
Noureddine Ben sadok <noureddine.bensadok1@gmail.com>
2025-04-25 11:22:41 +0200

    require

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
 dotenv.config();
 const app = express();
@@ -34,5 +34,6 @@ app.listen(PORT, () => {
 });
 
 app.get('/health', (req, res) => {
+    console.log('Health check endpoint hit');
     res.status(200).json({ message: 'Server is healthy' });
 });
