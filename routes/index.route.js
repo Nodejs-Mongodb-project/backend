edf86da623f51950e7047b175e5f499c87273e91
Noureddine Ben sadok <noureddine.bensadok1@gmail.com>
2025-05-19 11:47:41 +0200

    fix reservation

--- routes/index.route.js
+++ routes/index.route.js
@@ -2,8 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/auth', require('./auth.route'));
-router.use('/casier', require('./casier.route'));
-const reservationRoutes = require('./reservation.route');
+// Import routes
+const authRoute = require('./auth.route');
+const casierRoute = require('./casier.route');
+const reservationRoute = require('./reservation.route');
+
+router.use('/auth', authRoute);
+router.use('/casier', casierRoute);
+router.use('/reservation', reservationRoute);
 
 module.exports = router;
--- routes/reservation.route.js
+++ routes/reservation.route.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
-const { reserverCasier } = require('../controllers/reservation.controller');
+const {
+        reserverCasier,
+        getReservationsByUserId,
+        getReservationById,
+        cancelReservation,
+        getAllReservations,
+        getReservationByCasierId
+    } = require('../controller/reservation.controller');
 const { verifyToken } = require('../utils/auth.util');
 
-router.post('/reserver', verifyToken, reserverCasier); 
+router.post('/reserver', verifyToken, reserverCasier);
+router.get('/user/:userId', verifyToken, getReservationsByUserId);
+router.get('/:reservationId', verifyToken, getReservationById);
+router.delete('/:reservationId', verifyToken, cancelReservation);
+router.get('/all', verifyToken, getAllReservations);
+router.get('/casier/:casierId', verifyToken, getReservationByCasierId);
 
 module.exports = router;
